e505d01fd83eed993e10403fb8f5d41426440208
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2024-09-19 19:38:48 +0000

    Coordinate data types

---
+++ reference/main.lua
@@ -0,0 +1,24 @@
+local parser = require("parser")
+local treeDrawer = require("TreeDrawer")
+
+local function main()
+    -- Logic to display BNF grammar and take user input
+    while true do
+        print("Enter an input string (or 'HALT' to terminate):")
+        local input = io.read()
+        
+        if input == "HALT" then
+            break
+        end
+        
+        if parser.isValidExpression(input) then
+            print("Derivation successful!")
+            parser.rightmostDerivation(input)
+            treeDrawer.drawParseTree(input)
+        else
+            print("Error: Invalid input string.")
+        end
+    end
+end
+
+main()
