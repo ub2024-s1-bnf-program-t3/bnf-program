650763899bcb3482dfc7d00f8b8dd540092cd5ae
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2024-09-22 06:49:51 +0000

    Refactor REPL code to include BNF grammar rules and example string

--- main.lua
+++ main.lua
@@ -11,7 +11,16 @@ end
 while true do
     ::continue::
     print("\n--- REPL ---")
-    print("Enter 'HALT' to exit the program")
+    print("Welcome to the BNF Context-Free REPL!\n")
+    print("The following are the rules for this variation of the BNF grammar:\n")
+    print("<proc> → ON <body> OFF")
+    print("<instructions> → <line> | <line> - <instructions>")
+    print("<line> → sqr <xy>,<xy> | tri <xy>,<xy>,<xy>")
+    print("<xy> → <x><y>")
+    print("<x> → a | b | c | d | e | f")
+    print("<y> → 1 | 2 | 3 | 4 | 5 | 6")
+    print("\n\nAnd here is an example of an accepted string to try out: \"ON sqr b3,e5 - tri c3,a1,f6 OFF\"")
+    print("Note you can always enter 'HALT' to exit the program.\n\nEnjoy!\n")
     local input = getInput()
     if input == "HALT" then
         break
@@ -19,7 +28,7 @@ while true do
     local scanning_device = scanner:scan(input)
     -- print(scanning_device)
     -- Go through the array array and print the values
-    if(#scanning_device == 0) then
+    if (#scanning_device == 0) then
         print("Error in scanning. Please check your input.")
         goto continue
     end
@@ -33,16 +42,16 @@ while true do
     local parser = Parser:new(scanning_device, input)
     local ast = parser:parse()
     -- Print the AST for debugging purposes
-    print("--- AST ---")
     if ast ~= -1 then
+        print("--- AST ---")
         ast:print()
+        print("--- End of AST ---")
     end
-    print("--- End of AST ---")
-    print("--- Derivation ---")
     if ast == -1 then
         print("Error in parsing")
         goto continue
     end
+    print("--- Derivation ---")
     local derivator = Derivator:new(parser, ast)
     derivator:new_derivation()
     print("--- End of Derivation ---")
